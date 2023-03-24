8fa518c8c608c8b1a2e4ba3bf4a10b404046e0e9
Duc Do Trong <ducdotrong2000@gmail.com>
2023-03-25 02:16:02 +0700

    feat: chappter 1 and 2

---
+++ orm.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import mapper, relationship
+from sqlalchemy import MetaData, Table, Column, Integer, String
+
+import model
+
+metadata = MetaData()
+
+order_lines = Table(
+    'order_lines', metadata, Column(
+        'id', Integer, primary_key=True, autoincrement=True),
+    Column('sku', String(255)),
+    Column('qty', Integer, nullable=False),
+    Column('orderid', String(255))
+)
